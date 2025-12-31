8f66c6693d3b1295151144d772aceb96ef9d2b58
sajad <zareisajad@protonmail.com>
2025-12-31 22:23:59 +0330

    Add bootstrap

---
+++ .dotfiles/scripts/bootstrap.sh
@@ -0,0 +1,8 @@
+#!/bin/zsh
+
+sudo pacman -S --needed \
+    tmux telegram-desktop zsh zola nodejs npm nvim \
+    git picom obsidian okular mpv konsole obs-studio \
+    ttf-jetbrains-mono polybar pamixer feh i3status \
+    brightnessctl numlockx python-virtualenv xorg-xrandr
+
