620a6ac996587c53ad985eb470a94e98fa36ecfd
sajad <zareisajad@protonmail.com>
2026-01-26 18:27:11 +0330

    update dotfiles

--- .dotfiles/scripts/bootstrap.sh
+++ .dotfiles/scripts/bootstrap.sh
@@ -5,6 +5,6 @@ sudo pacman -Syu --needed --noconfirm
 sudo pacman -S --needed \
     tmux telegram-desktop zsh zola nodejs npm nvim \
     git picom obsidian okular mpv konsole obs-studio \
-    ttf-jetbrains-mono polybar pamixer feh xorg-xrandr \
+    ttf-jetbrains-mono  pamixer feh xorg-xrandr \
     i3status brightnessctl numlockx python-virtualenv python-pynvim \
     luarocks rust neovim 
